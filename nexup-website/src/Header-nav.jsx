7b9a256e565d7c1625e2c011642e1ccd49893e36
karthikgugulothu-dev <karthikgugulothu64@gmail.com>
2025-11-26 17:02:05 +0530

    Deleted Header-nav.jsx

--- nexup-website/src/Header-nav.jsx
+++ nexup-website/src/Header-nav.jsx
@@ -1,11 +1,3 @@
-import {createRoot} from 'react-dom/clinet'
-function App() {
     return ( 
-        <>
-        <h3>Header NexUp</h3>
-        </>
     )
-}
 createRoot(document.getElementById('root')).render(
-    <App/>
-)
